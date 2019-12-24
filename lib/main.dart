82a6312fa4c0e61213360792287c09d559f4b9b1
Regera <thinhdrak9@gmail.com>
2019-12-25 00:02:51 +0700

    Random words generator app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main(List<String> args) {
-  runApp(
-    new Center(
-      child: new Directionality(
-        textDirection: TextDirection.ltr,
-        child: new Text("Hello World")
-      )
-    )
-  );
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Random Words Generator",
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWordState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Random Words Generator"),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordState createState() => RandomWordState();
 }
