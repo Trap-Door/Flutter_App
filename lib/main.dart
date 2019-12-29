ef3f34c83cb8f0efd24a47acec96a36675f7eefa
Regera <thinhdrak9@gmail.com>
2019-12-29 23:19:33 +0700

    Shopping Cart App using Stateless/fulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,38 +3,104 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(
     MaterialApp(
-      title: "UI Tutorial App",
-      home: TutorialHome(),
+      title: "Shopdiiiiii",
+      home: ShoppingList(
+        productList: <Product>[
+          Product(name: "Ice scream"),
+          Product(name: "Toothpaste"),
+          Product(name: "Toothbrush"),
+          Product(name: "Vegetable")
+        ],
+      ),
     ),
   );
 }
 
-class TutorialHome extends StatelessWidget {
+class Product {
+  const Product({this.name});
+  final String name;
+}
+
+typedef void CartChangedCallback(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem({Product product, this.inCart, this.onCartChanged})
+      : product = product,
+        super(key: ObjectKey(product));
+
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  //Color and style for different parts of the tree
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getStyle(BuildContext context) {
+    if (!inCart) return null;
+
+    return TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onCartChanged(product, inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(
+        product.name,
+        style: _getStyle(context),
+      ),
+    );
+  }
+}
+
+class ShoppingList extends StatefulWidget {
+  ShoppingList({Key key, this.productList}) : super(key: key);
+
+  final List<Product> productList;
+
+  @override
+  ShoppingListState createState() => ShoppingListState();
+}
+
+class ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = Set<Product>();
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: "Menu",
-          onPressed: null,
-        ),
-        title: Text("UI Designer"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: "Search",
-            onPressed: null,
-          ),
-        ],
+        title: Text("Promotion Items"),
       ),
-      body: Center(
-        child: Text("Hello, Flutter"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: "Add",
-        child: Icon(Icons.add),
-        onPressed: null,
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.productList.map((Product product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
       ),
     );
   }
