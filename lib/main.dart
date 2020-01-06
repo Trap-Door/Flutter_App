d37bb07c04b38db3680142ebc4311e377df9d5ab
Regera <thinhdrak9@gmail.com>
2020-01-06 23:38:47 +0700

    Add interactivity to the app

--- lib/main.dart
+++ lib/main.dart
@@ -29,11 +29,7 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text("41")
+          FavoriteWidget(),
         ],
       ),
     );
@@ -46,6 +42,19 @@ class MyApp extends StatelessWidget {
           _buildButtonColumn(color, Icons.near_me, "ROUTE"),
           _buildButtonColumn(color, Icons.share, "SHARE")
         ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
       ),
     );
 
@@ -72,19 +81,6 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  Widget textSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Text(
-      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-      'Alps. Situated 1,578 meters above sea level, it is one of the '
-      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-      'half-hour walk through pastures and pine forest, leads you to the '
-      'lake, which warms to 20 degrees Celsius in the summer. Activities '
-      'enjoyed here include rowing, and riding the summer toboggan run.',
-      softWrap: true,
-    ),
-  );
-
   Column _buildButtonColumn(Color color, IconData icon, String label) => Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
@@ -103,4 +99,48 @@ class MyApp extends StatelessWidget {
           )
         ],
       );
+}
+
+class FavoriteWidget extends StatefulWidget {
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorite = true;
+  int _count = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorite) {
+        _isFavorite = false;
+        _count--;
+      } else {
+        _isFavorite = true;
+        _count++;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorite ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_count'),
+          ),
+        )
+      ],
+    );
+  }
 }
