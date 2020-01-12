d3f6a546dc33896fa177e394ec497fd646be6e57
Regera <thinhdrak9@gmail.com>
2020-01-13 00:04:38 +0700

    Animation example 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,146 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
-void main(List<String> args) => runApp(MyApp());
+void main() => runApp(LogoApp());
 
-class MyApp extends StatelessWidget {
+class LogoApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    Color color = Theme.of(context).primaryColor;
-
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    "Oeschinen Lake Campground",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(
-                  "Kandersteg, Switzerland",
-                  style: TextStyle(color: Colors.grey[500]),
-                ),
-              ],
-            ),
-          ),
-          FavoriteWidget(),
-        ],
-      ),
-    );
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          _buildButtonColumn(color, Icons.call, "CALL"),
-          _buildButtonColumn(color, Icons.near_me, "ROUTE"),
-          _buildButtonColumn(color, Icons.share, "SHARE")
-        ],
-      ),
-    );
-
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
-    return MaterialApp(
-      title: "Flutter Layout",
-      home: Scaffold(
-        // appBar: AppBar(
-        //   title: Text("Flutter Layout"),
-        // ),
-        body: ListView(
-          children: <Widget>[
-            Image.asset(
-              "images/lake.jpg",
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
-        ),
-      ),
-    );
-  }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) => Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(icon, color: color),
-          Container(
-            margin: const EdgeInsets.only(top: 8),
-            child: Text(
-              label,
-              style: TextStyle(
-                color: color,
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          )
-        ],
-      );
+  _LogoAppState createState() => _LogoAppState();
 }
 
-class FavoriteWidget extends StatefulWidget {
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
-}
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
 
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorite = true;
-  int _count = 41;
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorite) {
-        _isFavorite = false;
-        _count--;
-      } else {
-        _isFavorite = true;
-        _count++;
-      }
-    });
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            icon: (_isFavorite ? Icon(Icons.star) : Icon(Icons.star_border)),
-            color: Colors.red[500],
-            onPressed: _toggleFavorite,
-          ),
-        ),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child: Text('$_count'),
-          ),
-        )
-      ],
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
